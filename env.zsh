0b99fa49b105116a331fc99787e2a1fa547e2725
Christofer Lind <chris@Christofers-MacBook-Pro.local>
2024-08-24 15:46:43 +0200

    remove redundant env variables

--- env.zsh
+++
@@ -1,6 +0,0 @@
-export CI_PACKAGE_REGISTRY_PASSWORD="R-TirqU_LswgTpq9_b-x"
-
-export CI_PACKAGE_REGISTRY_PASSWORD="jHQxpqBESqqNzENipFPW"
-export CI_PACKAGE_REGISTRY_USER="gitlab+deploy-token-28"
-export VSS_NUGET_EXTERNAL_FEED_ENDPOINTS='{"endpointCredentials": [{"endpoint":"https://git.cygate.io/api/v4/groups/154/-/packages/nuget/index.json", "username":"'$CI_PACKAGE_REGISTRY_USER'", "password":"'$CI_PACKAGE_REGISTRY_PASSWORD'"}]}'
-
