d709cb9debb9bddf76bff87760cb64bdd433f597
Christofer Lind <chris@Christofers-MacBook-Pro.local>
2024-08-24 15:47:05 +0200

    add some helper aliases

--- global.zsh
+++ global.zsh
@@ -1,2 +1,5 @@
-alias sd='xrandr --output eDP-1 --brightness '
+# alias sd='xrandr --output eDP-1 --brightness '
 
+alias myip='dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com;'
+alias hs='history | grep'
+alias al='alias | grep'
